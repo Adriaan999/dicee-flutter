d53387c5cc3ad6ab65918645f1357a281892f85d
Adriaan van Schalkwyk <Adriaan9@hotmail.co.za>
2022-08-24 14:47:30 +0200

    recreate project

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
+  runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Text('I am Poor'),
+          backgroundColor: Colors.blueGrey.shade900,
         ),
-        body: DicePage(),
       ),
     ),
   );
-}
-
-class DicePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
